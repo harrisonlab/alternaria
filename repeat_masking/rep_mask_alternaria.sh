d816b89927fd01ab8160071a1131f68b3eb71f30
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-09-26 12:35:58 +0100

    Commands used to repeatmask Alternaria genomes on 26.09.14

---
+++ repeat_masking/rep_mask_alternaria.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._gaisen/650/650_assembly.41/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/635/635_assembly.41/
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/648/648_assembly.41/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/743/743_assembly.41/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/1082/1082_assembly.41/sorted_contigs.fa
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/1164/1164_assembly.41/sorted_contigs.fa
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/1166/ 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/1177/1177_assembly.41/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._tenuissima/24350/24350_assembly.41/sorted_contigs.fa
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._arborescens/675/675_assembly.41/sorted_contigs.fa
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._arborescens/97.0013/97.0013_assembly.41/sorted_contigs.fa
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/A.alternata_ssp._arborescens/97.0016/97.0016_assembly.41/sorted_contigs.fa
+
+
+
+
+
